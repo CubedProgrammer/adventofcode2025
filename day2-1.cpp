58cc935bba0146d5e23e382837e4df36948bb215
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2025-12-02 05:35:53 +0000

    day 2

---
+++ day2-1.cpp
@@ -0,0 +1,34 @@
+import std;
+using namespace std;
+using namespace literals;
+using namespace views;
+using ranges::to;
+auto solve(const string& input)
+{
+    auto check = [](long n)
+    {
+        string s = format("{}", n);
+        return s.size() % 2 == 0 && s.substr(0, s.size() / 2) == s.substr(s.size() / 2);
+    };
+    auto partials = split(input, ","sv) | to<vector<string>>() | views::transform([check](const string&s){
+        auto ind = s.find('-');
+        auto repeats = views::iota(stol(s.substr(0, ind)), 1+stol(s.substr(ind + 1))) | filter(check);
+        return ranges::fold_left(repeats, 0l, plus<>());
+    });
+    return ranges::fold_left(partials, 0l, plus<>());
+}
+string read_entire_file(std::istream& is)
+{
+    string s;
+    array<char, 1024>buf;
+    while(is.read(buf.data(), buf.size()))
+        s.append_range(take(buf, is.gcount()));
+    s.append_range(take(buf, is.gcount()));
+    return s;
+}
+int main(int argl,char**argv)
+{
+    string i = read_entire_file(cin);
+    println("{}", solve(i));
+    return 0;
+}
